abcb838f038a1b2627262599b2d8c988523926f1
godgodwinter <kakadz.pro@gmail.com>
2022-02-02 11:11:34 +0700

    add menu jabatan

--- app/Http/Controllers/adminjabatancontroller.php
+++ app/Http/Controllers/adminjabatancontroller.php
@@ -2,9 +2,92 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\Fungsi;
+use App\Models\jabatan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class adminjabatancontroller extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        #WAJIB
+        $pages='jabatan';
+        $datas=jabatan::paginate(Fungsi::paginationjml());
+
+        return view('pages.admin.jabatan.index',compact('datas','request','pages'));
+    }
+    public function cari(Request $request)
+    {
+        $cari=$request->cari;
+        #WAJIB
+        $pages='jabatan';
+        $datas=jabatan::where('nama','like',"%".$cari."%")
+        ->paginate(Fungsi::paginationjml());
+
+        return view('pages.admin.jabatan.index',compact('datas','request','pages'));
+    }
+    public function create()
+    {
+        $pages='jabatan';
+
+        return view('pages.admin.jabatan.create',compact('pages'));
+    }
+
+    public function store(Request $request)
+    {
+
+        // dd($request);
+            $request->validate([
+                'nama'=>'required',
+
+            ],
+            [
+                'nama.nama'=>'Nama harus diisi',
+            ]);
+
+            $getid=DB::table('jabatan')->insertGetId(
+                array(
+                       'nama'     =>   $request->nama,
+                       'created_at'=>date("Y-m-d H:i:s"),
+                       'updated_at'=>date("Y-m-d H:i:s")
+                ));
+
+    return redirect()->route('jabatan')->with('status','Data berhasil tambahkan!')->with('tipe','success')->with('icon','fas fa-feather');
+
+    }
+
+    public function edit(jabatan $id)
+    {
+        $pages='jabatan';
+
+        return view('pages.admin.jabatan.edit',compact('pages','id'));
+    }
+    public function update(jabatan $id,Request $request)
+    {
+
+
+        $request->validate([
+            'nama'=>'required',
+        ],
+        [
+            'nama.required'=>'name harus diisi',
+        ]);
+
+
+            jabatan::where('id',$id->id)
+            ->update([
+                'nama'     =>   $request->nama,
+               'updated_at'=>date("Y-m-d H:i:s")
+            ]);
+
+
+    return redirect()->route('jabatan')->with('status','Data berhasil diubah!')->with('tipe','success')->with('icon','fas fa-feather');
+    }
+    public function destroy(jabatan $id){
+
+        jabatan::destroy($id->id);
+        return redirect()->route('jabatan')->with('status','Data berhasil dihapus!')->with('tipe','warning')->with('icon','fas fa-feather');
+
+    }
 }
---
+++ resources/views/pages/admin/jabatan/edit.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.gentella')
+
+@section('title')
+Jabatan
+@endsection
+
+@push('before-script')
+@if (session('status'))
+<x-sweetalertsession tipe="{{session('tipe')}}" status="{{session('status')}}" />
+@endif
+@endpush
+
+
+@section('content')
+
+    <div class="">
+        <div class="page-title">
+            <div class="title_left">
+                {{-- <h3>@yield('title')</h3> --}}
+            </div>
+            {{-- <div class="title_right">
+                <div class="col-md-5 col-sm-5 form-group pull-right top_search">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search for...">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">Go!</button>
+                        </span>
+                    </div>
+                </div>
+            </div> --}}
+        </div>
+        <div class="clearfix"></div>
+        <div class="row">
+            <div class="col-md-12 col-sm-12">
+                <div class="x_panel">
+                    <div class="x_title">
+                        <h2>@yield('title')</h2>
+                        <ul class="nav navbar-right panel_toolbox">
+                            {{-- <li><a class="close-link"><i class="fa fa-close"></i></a>
+                            </li> --}}
+                        </ul>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="x_content">
+                        <form  action="{{route('jabatan.update',$id->id)}}" method="post" >
+                            @method('put')
+                            @csrf
+                            <div class="field item form-group">
+                                <label class="col-form-label col-md-3 col-sm-3  label-align">Nama<span
+                                        class="required">*</span></label>
+                                <div class="col-md-6 col-sm-6">
+                                    <input class="form-control"
+                                        name="nama" id="nama"  required="required" value="{{old('nama')?old('nama'):$id->nama}}" />
+                                </div>
+                            </div>
+                            <div class="ln_solid">
+                                <div class="form-group">
+                                    <div class="col-md-6 offset-md-3">
+                                        <button type='submit' class="btn btn-primary">Simpan</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
